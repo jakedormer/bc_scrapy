f489d2cae54b70c52d3f0d32261979ad505af050
Jake <contact@basketcompare.co.uk>
2019-08-29 23:02:43 +0100

    promo regx fix

--- basketcompare/pipelines.py
+++ basketcompare/pipelines.py
@@ -9,91 +9,87 @@ import json
 class PriceItemPipeline(object):
 
 	def regex_promo(self, shelf_price, promotion):
-	    # print("£",shelf_price,", ", promotion)
-	    # x for £xx
-	    x = re.search('(?!.*?each)([0-9]+) for £(\d*\.?\d*).*', promotion, re.IGNORECASE)
-	    if x:
-	        x1 = round(float(x.group(2)) / float(x.group(1)), 2)
-	        print(x1)
-	        return x1
-	        
-	     # x or more for £x.xx?
-	    x = re.search('.*[0-9]+ or more for £(\d*\.?\d*).*', promotion, re.IGNORECASE)
-	    if x:
-	        x1 = round(float(x.group(1)), 2)
-	        print(x1)
-	        return x1
-	        
-	        
-	    # £x.xx each when you buy x or more
-	    x = re.search('.*£(\d*\.?\d*) each', promotion, re.IGNORECASE)
-	    if x:
-	        x1 = round(float(x.group(1)), 2)
-	        print(x1)
-	        return x1
-	    
-	    # Buy 1 get 1 free
-	    x = re.search('(buy (one|1) get (one|1) free|bogof)', promotion, re.IGNORECASE)
-	    if x:
-	        x1 = round(shelf_price / 2, 2)
-	        print(x1)
-	        return x1
-	    
-	   
-	    # x for (the price of)? x
-	    x = re.search('.*([3-5]) for (the price of )?([2-4]).*', promotion, re.IGNORECASE)
-	    if x:
-	        if x.group(3):
-	            x1 = round(shelf_price * (float(x.group(3)) / float(x.group(1))), 2)
-	            print(x1)
-	            return x1
-	        else:
-	            x1 = round(shelf_price * (float(x.group(2)) / float(x.group(1))), 2)
-	            print(x1)
-	            return x1
-	    
-	    # xx% off when you spend £x +
-	    x = re.search('([0-9]+)% off.*when you spend.* £(\d*\.?\d*).*', promotion, re.IGNORECASE)
-	    if x:
-	        if shelf_price > float(x.group(2)):
-	            x1 = round(shelf_price * (1 - float(x.group(1)) / 100), 2)
-	            print(x1)
-	            return x1
-	        else:
-	            print(shelf_price)
-	            return shelf_price
-	    
-	    # £x off when you spend £x +
-	    x = re.search('£(\d*\.?\d*).*when you spend.*£(\d*\.?\d*).*', promotion, re.IGNORECASE)
-	    if x:
-	        if shelf_price > float(x.group(2)):
-	            x1 = round(shelf_price - float(x.group(1)), 2)
-	            print(x1)
-	            return x1
-	        else:
-	            print(shelf_price)
-	            return shelf_price
+	# print("£",shelf_price,", ", promotion)
+	# x for £xx
+		x = re.search('(?!.*?each)([0-9]+) for £(\d*\.?\d*).*', promotion, re.IGNORECASE)
+		if x:
+			x1 = round(float(x.group(2)) / float(x.group(1)), 2)
+			print(x1)
+			return x1
+			
+		 # x or more for £x.xx?
+		x = re.search('.*[0-9]+ or more for £(\d*\.?\d*).*', promotion, re.IGNORECASE)
+		if x:
+			x1 = round(float(x.group(1)), 2)
+			print(x1)
+			return x1
+			
+			
+		# £x.xx each when you buy x or more
+		x = re.search('.*£(\d*\.?\d*) each', promotion, re.IGNORECASE)
+		if x:
+			x1 = round(float(x.group(1)), 2)
+			print(x1)
+			return x1
 
-	    # Buy 1 get 1 half price
-	    x = re.search('buy (one|1).*half price.*', promotion, re.IGNORECASE)
-	    if x:
-	        x1 = round(shelf_price * 0.75, 2)
-	        print(x1)
-	        return x1
-	    else:
-	        print(shelf_price)
-	        return shelf_price
-	    
-	    
-	    # xx% off, discount applied at checkout
-	    x = re.search('([0-9]+)% off.*checkout.*', promotion, re.IGNORECASE)
-	    if x:
-	        x1 = round(shelf_price * (1 - float(x.group(1)) / 100), 2)
-	        print(x1)
-	        return x1
-	    else:
-	        print(shelf_price)
-	        return shelf_price
+		# Buy 1 get 1 free
+		x = re.search('(buy (one|1) get (one|1) free|bogof)', promotion, re.IGNORECASE)
+		if x:
+			x1 = round(shelf_price / 2, 2)
+			print(x1)
+			return x1
+
+
+		# x for (the price of)? x
+		x = re.search('.*([3-5]) for (the price of )?([2-4]).*', promotion, re.IGNORECASE)
+		if x:
+			if x.group(3):
+				x1 = round(shelf_price * (float(x.group(3)) / float(x.group(1))), 2)
+				print(x1)
+				return x1
+			else:
+				x1 = round(shelf_price * (float(x.group(2)) / float(x.group(1))), 2)
+				print(x1)
+				return x1
+
+		# xx% off when you spend £x +
+		x = re.search('([0-9]+)% off.*when you spend.* £(\d*\.?\d*).*', promotion, re.IGNORECASE)
+		if x:
+			if shelf_price > float(x.group(2)):
+				x1 = round(shelf_price * (1 - float(x.group(1)) / 100), 2)
+				print(x1)
+				return x1
+			else:
+				print(shelf_price)
+				return shelf_price
+
+		# £x off when you spend £x +
+		x = re.search('£(\d*\.?\d*).*when you spend.*£(\d*\.?\d*).*', promotion, re.IGNORECASE)
+		if x:
+			if shelf_price > float(x.group(2)):
+				x1 = round(shelf_price - float(x.group(1)), 2)
+				print(x1)
+				return x1
+			else:
+				print(shelf_price)
+				return shelf_price
+
+		# Buy 1 get 1 half price
+		x = re.search('buy (one|1).*half price.*', promotion, re.IGNORECASE)
+		if x:
+			x1 = round(shelf_price * 0.75, 2)
+			print(x1)
+			return x1
+
+		# xx% off, discount applied at checkout
+		x = re.search('([0-9]+)% off.*checkout.*', promotion, re.IGNORECASE)
+		if x:
+			x1 = round(shelf_price * (1 - float(x.group(1)) / 100), 2)
+			print(x1)
+			return x1
+		else:
+			print(shelf_price)
+			return shelf_price
 
 	def process_item(self, item, spider):
 		# To Remove lists
